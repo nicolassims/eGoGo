2eeacc71c07ec631f85b1c6c43fffac80d27bd01
nicolassims <sableye.nico@gmail.com>
2016-10-04 14:48:50 -0400

    Trying to fix text error where print knowself reaction isn't printing.

--- main.js
+++ main.js
@@ -8,15 +8,17 @@
 "use strict";
 const PROMPT = require('readline-sync');
 
-let fName, lName;
+let chooseVar, fName, lName;
 
 function main() {
     printIntro();
     setFName();
     setLName();
     printNameReaction();
-    knowSelf();
-    knowPurpose();
+    setKnowSelf();
+    printKnowSelf();
+    setKnowPurpose();
+    printKnowPurpose();
 }
 
 main();
@@ -41,51 +43,43 @@ function printNameReaction() {
         'I\'d imagine not, but why don\'t you tell me?');
 }
 
-function knowSelf() {
-    let knowSelf = PROMPT.question('<<(1) = I know who you are.>>\n' +
+function setKnowSelf() {
+    let chooseVar = PROMPT.question('<<(1) = I know who you are.>>\n' +
         '<<(2) = I do not know you.>>\n' +
         '<<(3) = I do not know you, nor do I care to.>>\n' +
         '>');
-    while (knowSelf != 1 && knowSelf != 2 && knowSelf != 3) {
-        knowSelf = PROMPT.question('That is not an option.\n' +
+    while (chooseVar != 1 && chooseVar != 2 && chooseVar != 3) {
+        chooseVar = PROMPT.question('That is not an option.\n' +
             '<<(1) = I know who you are.>>\n' +
             '<<(2) = I do not know you.>>\n' +
             '<<(3) = I do not know you, nor do I care to.>>\n' +
             '>');
     }
-    printKnowSelf(knowSelf);
 }
 
 function printKnowSelf() {
-    if (knowSelf == 1) {
+    if (chooseVar == 1) {
         console.log('\nFascinating.\n ' +
             'If you know me, then you must truly know yourself.\n ' +
-            'An enviable quality.\n ' +
-            'Doubtlessly, though, you do not know why I am here.\n ' +
-            'Or why you are here, for that matter.\n ' +
-            'Do you?');
-    } else if (knowSelf == 2) {
+            'An enviable quality.\n ');
+    } else if (chooseVar == 2) {
         console.log('\nFew people do.\n ' +
             'Pay it no mind.\n ' +
             'It is not a personal failing.\n ' +
-            'You will, in time, learn of me.\n ' +
-            'Doubtlessly, though, you do not know why I am here.\n ' +
-            'Or why you are here, for that matter.\n ' +
-            'Do you?');
-
-    } else {
+            'You will, in time, learn of me.\n ');
+    } else if (chooseVar == 3) {
         console.log('\n...?\n ' +
             'I see.\n ' +
             'Then be warned, arrogant one...\n ' +
-            'You will, in time, learn of me.\n ' +
-            'Doubtlessly, though, you do not know why I am here.\n ' +
-            'Or why you are here, for that matter.\n ' +
-            'Do you?');
+            'You will, in time, learn of me.\n ');
     }
+    console.log('\nDoubtlessly, though, you do not know why I am here.\n ' +
+        'Or why you are here, for that matter.\n ' +
+        'Do you?');
 }
 
-function knowPurpose() {
-    let knowSelf = PROMPT.question('<<(1) = I am here to find that which has not been found.>>\n' +
+function setKnowPurpose() {
+    chooseVar = PROMPT.question('<<(1) = I am here to find that which has not been found.>>\n' +
         '<<(2) = I am here to pacify that which is enraged.>>\n' +
         '<<(3) = I am here to do that which I can do.>>\n' +
         '<<(4) = I am here to think that which has not been thought.>>\n' +
@@ -98,7 +92,33 @@ function knowPurpose() {
         '<<(11) = I am here to befriend that which suffers alone.>>\n' +
         '<<(12) = I am here to console that which feels great sadness.>>\n' +
         '>');
-    while (knowSelf != 1 && knowSelf != 2 && knowSelf != 3) {
-        knowSelf = PROMPT.question('That is not an option.\n<<(1) = I know who you are.>>\n<<(2) = I do not know you.>>\n<<(3) = I do not know you, nor do I care to.>>\n>');
+    while (chooseVar != 1 && chooseVar != 2 && chooseVar != 3 && chooseVar != 4 && chooseVar != 5 && chooseVar != 6
+        && chooseVar != 7 && chooseVar != 8 && chooseVar != 9 && chooseVar != 10 && chooseVar != 11
+        && chooseVar != 12) {
+        chooseVar = PROMPT.question('\nThat is not an option.\n' +
+            '<<(1) = I am here to find that which has not been found.>>\n' +
+            '<<(2) = I am here to pacify that which is enraged.>>\n' +
+            '<<(3) = I am here to do that which I can do.>>\n' +
+            '<<(4) = I am here to think that which has not been thought.>>\n' +
+            '<<(5) = I am here to command that which needs direction.>>\n' +
+            '<<(6) = I am here to learn that which is not known.>>\n' +
+            '<<(7) = I am here to control that which has no law.>>\n' +
+            '<<(8) = I am here to enflame that which has no passion.>>\n' +
+            '<<(9) = I am here to empower that which has lost all hope.>>\n' +
+            '<<(10) = I am here to wait for that which will eventually come.>>\n' +
+            '<<(11) = I am here to befriend that which suffers alone.>>\n' +
+            '<<(12) = I am here to console that which feels great sadness.>>\n' +
+            '>');
+    }
+}
+
+function printKnowPurpose() {
+    console.log('\nFascinating. ');
+    if (chooseVar == 1) {
+        console.log('Then I wish you luck in your search.');
+    } else if (chooseVar == 2) {
+        console.log('Then I wish you a cool head.');
+    } else {
+
     }
 }
